92735df6a68867b5dab5b9d3b85bf28cb8b64854
oncomouse <oncomouse@gmail.com>
2020-09-15 09:29:51 -0500

    Cleaned up some variables

--- pythonx/ncm2_lsp.py
+++ pythonx/ncm2_lsp.py
@@ -1,34 +1,35 @@
-import re
 import json
+import re
+
 import vim
 from ncm2 import Ncm2Source
 
 LSP_KINDS = [
-    'Text',
-    'Method',
-    'Function',
-    'Constructor',
-    'Field',
-    'Variable',
-    'Class',
-    'Interface',
-    'Module',
-    'Property',
-    'Unit',
-    'Value',
-    'Enum',
-    'Keyword',
-    'Snippet',
-    'Color',
-    'File',
-    'Reference',
-    'Folder',
-    'EnumMember',
-    'Constant',
-    'Struct',
-    'Event',
-    'Operator',
-    'TypeParameter',
+    "Text",
+    "Method",
+    "Function",
+    "Constructor",
+    "Field",
+    "Variable",
+    "Class",
+    "Interface",
+    "Module",
+    "Property",
+    "Unit",
+    "Value",
+    "Enum",
+    "Keyword",
+    "Snippet",
+    "Color",
+    "File",
+    "Reference",
+    "Folder",
+    "EnumMember",
+    "Constant",
+    "Struct",
+    "Event",
+    "Operator",
+    "TypeParameter",
 ]
 
 
@@ -36,90 +37,92 @@ class Source(Ncm2Source):
     def __init__(self, vim):
         Ncm2Source.__init__(self, vim)
         self.vim = vim
-        self.vim.vars['ncm2_lsp#_results'] = []
-        self.vim.vars['ncm2_lsp#_success'] = False
-        self.vim.vars['ncm2_lsp#_requested'] = False
-        self.vim.vars['ncm2_lsp#_prev_input'] = ''
+        self.vim.vars["ncm2_lsp#_results"] = []
+        self.vim.vars["ncm2_lsp#_success"] = False
+        self.vim.vars["ncm2_lsp#_requested"] = False
+        self.vim.vars["ncm2_lsp#_prev_input"] = ""
 
     def on_complete(self, context):
         candidates = []
-        if self.vim.call('has', 'nvim-0.5.0'):
-            prev_input = self.vim.vars['ncm2_lsp#_prev_input']
-            if context['base'] == prev_input and self.vim.vars[
-                    'ncm2_lsp#_requested']:
+        if self.vim.call("has", "nvim-0.5.0"):
+            prev_input = self.vim.vars["ncm2_lsp#_prev_input"]
+            if context["base"] == prev_input and self.vim.vars["ncm2_lsp#_requested"]:
                 candidates = self.process_candidates()
             else:
-                vars = self.vim.vars
-                vars['ncm2_lsp#_requested'] = False
-                vars['ncm2_lsp#_prev_input'] = context['base']
-                vars['ncm2_lsp#_complete_position'] = context['startccol']
+                self.vim.vars["ncm2_lsp#_requested"] = False
+                self.vim.vars["ncm2_lsp#_prev_input"] = context["base"]
+                self.vim.vars["ncm2_lsp#_complete_position"] = context["startccol"]
 
-                params = self.vim.call('luaeval',
-                                       'vim.lsp.util.make_position_params()')
+                params = self.vim.call(
+                    "luaeval", "vim.lsp.util.make_position_params()")
 
                 self.vim.call(
-                    'luaeval', 'require("candidates").request_candidates('
-                    '_A.arguments)', {'arguments': params})
+                    "luaeval",
+                    'require("candidates").request_candidates(' "_A.arguments)",
+                    {"arguments": params},
+                )
 
         self.complete(context, context["startccol"], candidates)
 
     def process_candidates(self):
         candidates = []
-        vars = self.vim.vars
-        results = vars['ncm2_lsp#_results']
+        results = self.vim.vars["ncm2_lsp#_results"]
         if not results:
             return []
-        elif isinstance(results, dict):
-            if 'items' not in results:
+        if isinstance(results, dict):
+            if "items" not in results:
                 self.print_error(
                     'LSP results does not have "items" key:{}'.format(
-                        str(results)))
+                        str(results))
+                )
                 return []
-            items = results['items']
+            items = results["items"]
         else:
             items = results
         for rec in items:
-            if 'textEdit' in rec and rec['textEdit'] is not None:
-                textEdit = rec['textEdit']
-                if textEdit['range']['start'] == textEdit['range']['end']:
-                    previous_input = vars['ncm2_lsp#_prev_input']
-                    complete_position = vars['ncm2_lsp#_complete_position']
-                    new_text = textEdit['newText']
-                    word = f'{previous_input[complete_position:]}{new_text}'
+            if "textEdit" in rec and rec["textEdit"] is not None:
+                textEdit = rec["textEdit"]
+                if textEdit["range"]["start"] == textEdit["range"]["end"]:
+                    previous_input = self.vim.vars["ncm2_lsp#_prev_input"]
+                    complete_position = self.vim.vars["ncm2_lsp#_complete_position"]
+                    new_text = textEdit["newText"]
+                    word = f"{previous_input[complete_position:]}{new_text}"
                 else:
-                    word = textEdit['newText']
-            elif rec.get('insertText', ''):
-                if rec.get('insertTextFormat', 1) != 1:
-                    word = rec.get('entryName', rec.get('label'))
+                    word = textEdit["newText"]
+            elif rec.get("insertText", ""):
+                if rec.get("insertTextFormat", 1) != 1:
+                    word = rec.get("entryName", rec.get("label"))
                 else:
-                    word = rec['insertText']
+                    word = rec["insertText"]
             else:
-                word = rec.get('entryName', rec.get('label'))
+                word = rec.get("entryName", rec.get("label"))
 
             # Remove parentheses from word.
             # Note: some LSP includes snippet parentheses in word(newText)
-            word = re.sub(r'[\(|<].*[\)|>](\$\d+)?', '', word)
+            word = re.sub(r"[\(|<].*[\)|>](\$\d+)?", "", word)
 
             item = {
-                'word': word,
-                'abbr': rec['label'],
-                'dup': 0,
-                'user_data': json.dumps({'lspitem': rec})
+                "word": word,
+                "abbr": rec["label"],
+                "dup": 0,
+                "user_data": json.dumps({"lspitem": rec}),
             }
 
-            if isinstance(rec.get('kind'), int):
-                item['kind'] = LSP_KINDS[rec['kind'] - 1]
-            elif rec.get('insertTextFormat') == 2:
-                item['kind'] = 'Snippet'
+            if isinstance(rec.get("kind"), int):
+                item["kind"] = LSP_KINDS[rec["kind"] - 1]
+            elif rec.get("insertTextFormat") == 2:
+                item["kind"] = "Snippet"
 
-            if rec.get('detail'):
-                item['menu'] = rec['detail']
+            if rec.get("detail"):
+                item["menu"] = rec["detail"]
 
-            if isinstance(rec.get('documentation'), str):
-                item['info'] = rec['documentation']
-            elif (isinstance(rec.get('documentation'), dict)
-                  and 'value' in rec['documentation']):
-                item['info'] = rec['documentation']['value']
+            if isinstance(rec.get("documentation"), str):
+                item["info"] = rec["documentation"]
+            elif (
+                isinstance(rec.get("documentation"), dict)
+                and "value" in rec["documentation"]
+            ):
+                item["info"] = rec["documentation"]["value"]
 
             candidates.append(item)
 
